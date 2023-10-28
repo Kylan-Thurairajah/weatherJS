77d2aba042885404ed391b53bde389a3f6e2c8ff
Kylan-Thurairajah <kylan.r.thurairajah@gmail.com>
2023-10-28 14:59:14 -0400

    not registering search click

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const apiKey = "ee323acdce622706c557bdf4f39071a8";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=ottawa";
 
-async function checkWeather() {
-    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+const searchBox = document.querySelector(".search input")
+const searchBtn = document.querySelector(".search button")
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city +  `&appid=${apiKey}`);
     var data = await response.json();
 
     console.log(data);
@@ -14,4 +17,10 @@ async function checkWeather() {
 
 }
 
-checkWeather();
+
+
+window.onload=function(){
+    searchBtn.addEventListener("click", ()=>{
+        checkWeather(searchBox.value); // passing the city that was searched when search button clicked
+    })
+  }
